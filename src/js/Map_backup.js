f30d79588d024531233a7dc4a1d9f847e0709c0f
Bence Kiss-Dobronyi <bence.k@gmail.com>
2016-08-01 11:24:44 +0200

    Initial commit
    SF bike sharing map based on Chris Henrik's React-Leaflet demo

--- src/js/Map_backup.js
+++ src/js/Map_backup.js
@@ -4,7 +4,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var L = require('leaflet');
 var qwest = require('qwest');
-var Baby = require('babyparse');
+//var Baby = require('babyparse');
 
 // add our subway line filter component
 var Filter = require('./Filter');
@@ -17,16 +17,16 @@ var config = {};
 var tmpSubwayLines = [];
 // tmp array used to eventually create the above array
 var subwayLines = [];
-// tmp json for trafficData
+// tmp json for csvdata
 
-console.log("4.43");
+console.log("4.38");
 
 // map paramaters to pass to L.map when we instantiate it
 config.params = {
     center: [40.655769, -73.938503], //Greenpoint
     zoomControl: false,
     zoom: 13,
-    maxZoom: 16,
+    maxZoom: 19,
     minZoom: 7,
     scrollwheel: false,
     legends: true,
@@ -55,8 +55,7 @@ var Map = React.createClass({
             geojsonLayer: null,
             geojson: null,
             filter: '*',
-            numEntrances: null,
-            trafficData: null
+            numEntrances: null
         };
     },
 
@@ -70,7 +69,18 @@ var Map = React.createClass({
         // code to run just before adding the map
     },
 
-    componentDidMount: function () { 
+    componentDidMount: function () {
+        console.log(this);
+//        console.log('componentDidMount')
+//        console.log('isfetchingData: ' + this.fetchingData);
+//        console.log('isfetchingTrafficData: ' + this.fetchingTrafficData);    
+        // code to run just after the component "mounts" / DOM elements are created
+        // read csv data
+//        console.log('isItfecthingtraffic: ' + (!this.fetchingTrafficData && this.state.csvdata == null));
+        if (!this.fetchingTrafficData && this.state.csvdata == null) this.getTrafficData();
+        // make the AJAX request for the GeoJSON data
+//        console.log('isfetchingData: ' + this.fetchingData);
+//        console.log('isfetchingTrafficData: ' + this.fetchingTrafficData);    
         if (!this.fetchingData) this.getData();
         // create the Leaflet map object
         if (!this.map) this.init(this.getID());
@@ -109,38 +119,55 @@ var Map = React.createClass({
     },
 
     getData: function () {
+        console.log("getData started");
         this.fetchingData = true;
         var that = this;
+//        console.log(that.state.csvdata);
         
+
+        // qwest is a library for making Ajax requests, we use it here to load GeoJSON data
+        qwest.get('station_data.geojson', null, {
+                responseType: 'json'
+            })
+            .then(function (xhr, res) {
+                if (that.isMounted()) {
+                    // store the number of GeoJSON features (subway entrances) in the component's state for use later
+                    // as well as the raw GeoJSON data so it can be reused
+                    that.setState({
+                        numEntrances: res.features.length,
+                        geojson: res
+                    });
+                }
+                that.fetchingData = false;
+            console.log('getData ended');
+            })
+            .catch(function (xhr, res, e) {
+                console.log('qwest catch: ', xhr, res, e);
+            });
+    },
+    
+    getTrafficData: function () {
+        isFetchingTrafficData = true;
+        console.log('fetchtrafficdata_started');
+        this.fetchingTrafficData = true;
+        console.log(this.fetchingTrafficData);
+        var that = this;
+
         qwest.get('201508_trip_data_sample.csv', null, {
             responseType: 'text'
         })
-        .then(function (xhr_a, res_a) {
+        .then(function (xhr, res) {
             if(that.isMounted()){
-                var traffic = Baby.parse(res_a, {header: true});
+                var traffic = Baby.parse(res, {header: true});
                 that.setState({
-                    trafficData: traffic
+                    csvdata: traffic
                 });
-                qwest.get('station_data.geojson', null, {
-                responseType: 'json'
-                    })
-                    .then(function (xhr_b, res_b) {
-                        if (that.isMounted()) {
-                            // store the number of GeoJSON features (subway entrances) in the component's state for use later
-                            // as well as the raw GeoJSON data so it can be reused
-                            that.setState({
-                                numEntrances: res_b.features.length,
-                                geojson: res_b
-                            });
-                        }
-                    })
-                    .catch(function (xhr_b, res_b, e) {
-                        console.log('qwest catch: ', xhr, res, e);
-                    });
             }
-            that.fetchingData = false;
+            that.fetchingTrafficData = false;
+            console.log('fetchtrafficdata_ended');
+            console.log(that.state.csvdata);
+            isFetchingTrafficData = false;
         });
-
     },
 
     addGeoJSONLayer: function (geojson) {
@@ -200,76 +227,71 @@ var Map = React.createClass({
     },
 
     pointToLayer: function (feature, latlng) {
+        this.fetchingTrafficData = true;
         this.fetchingData = true;
         // renders our GeoJSON points as circle markers, rather than Leaflet's default image markers
+    
+//        function getSumOfTraffic(station_id){
+//            if(this.state.csvdata != null){
+//            var tmpSum = 0;
+//                for(i=0; i < this.state.csvdata.data.length; i++){
+//                    console.log(this.state.csvdata.data[i].end_id);
+//                    if(this.state.csvdata.data[i].end_id == station_id){
+//                        tmpSum += 1;
+//                    }
+//                }
+//            }
+//            return station_id;
+//        };
         
-        function getSumOfTraffic(station_id, thisIn){
-            var that = thisIn;
-            if(that.state.trafficData != null){
-                var tmpSum = 0;
-                for(i=0; i < that.state.trafficData.data.length; i++){
-//                    console.log(that.state.trafficData.data[i].end_id);
-                    if(that.state.trafficData.data[i].end_id == station_id){
-                        tmpSum += 1;
-                    }
-                }
-            }
-            return tmpSum;
-        };
-        
-        function getColor(d) {
-            return d > 500 ? '#800026' :
-                   d > 400  ? '#BD0026' :
-                   d > 300  ? '#E31A1C' :
-                   d > 200  ? '#FC4E2A' :
-                   d > 100   ? '#FD8D3C' :
-                   d > 50   ? '#FEB24C' :
-                   d > 20   ? '#FED976' :
-                              '#FFEDA0';
-        };
+//        function far(x){
+//            return 2;
+////            return getSumOfTraffic(2);
+////          return parseInt(this.state.csvdata.data[1].end_id);  
+//        };
         
 //        var sumOfTraffic = far(feature.properties.station_id);
         // parameters to style the GeoJSON markers
-        var sumofin = getSumOfTraffic(feature.properties.station_id, this);
-        feature.properties.sumofIn = sumofin;    
         var markerParams = {
-            radius: Math.log(sumofin)*5,
-            fillColor: getColor(sumofin),
+            radius: 9,
+            fillColor: 'orange',
             color: '#fff',
             weight: 1,
             opacity: 0.5,
             fillOpacity: 0.8
         };
+
         return L.circleMarker(latlng, markerParams);
     },
 
     onEachFeature: function (feature, layer) {
         // this method is used to create popups for the GeoJSON features
         // it also creates the initial array of unique subway line names which is later passed to the Filter component
-        if (feature.properties) {
+
+        if (feature.properties && feature.properties.NAME && feature.properties.LINE) {
             // if the array for unique subway line names has not been made, create it
-//            if (subwayLines.length === 0) {
-//                // add subway line names to a temporary subway lines array
-//                feature.properties.LINE.split('-').forEach(function (line, index) {
-//                    tmpSubwayLines.push(line);
-//                });
-//
-//                // on the last GeoJSON feature make a new array of unique values from the temporary array
-//                if (this.state.geojson.features.indexOf(feature) === this.state.numEntrances - 1) {
-//                    // use filter() to make sure the subway line names array has one value for each subway line
-//                    // use sort() to put our values in numeric and alphabetical order
-//                    subwayLines = tmpSubwayLines.filter(function (value, index, self) {
-//                        return self.indexOf(value) === index;
-//                    }).sort();
-//                    // finally add a value to represent all of the subway lines
-//                    subwayLines.unshift('All lines');
-//                }
-//            }
+            if (subwayLines.length === 0) {
+                // add subway line names to a temporary subway lines array
+                feature.properties.LINE.split('-').forEach(function (line, index) {
+                    tmpSubwayLines.push(line);
+                });
+
+                // on the last GeoJSON feature make a new array of unique values from the temporary array
+                if (this.state.geojson.features.indexOf(feature) === this.state.numEntrances - 1) {
+                    // use filter() to make sure the subway line names array has one value for each subway line
+                    // use sort() to put our values in numeric and alphabetical order
+                    subwayLines = tmpSubwayLines.filter(function (value, index, self) {
+                        return self.indexOf(value) === index;
+                    }).sort();
+                    // finally add a value to represent all of the subway lines
+                    subwayLines.unshift('All lines');
+                }
+            }
 
             // assemble the HTML for the markers' popups
-            var popupContent = '<h3>Station: ' + feature.properties.name +
-                '</h3><strong>Incoming bikes (in period):</strong> ' +
-                feature.properties.sumofIn.toString();
+            var popupContent = '<h3>' + feature.properties.NAME +
+                '</h3><strong>Access to MTA lines:</strong> ' +
+                feature.properties.LINE;
             // add our popups
             layer.bindPopup(popupContent);
         }
